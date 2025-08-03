419c7d29f205ee388e26afd9fc8982f8d573bc33
DulakshaDDSM <114049877+DulakshaDDSM@users.noreply.github.com>
2025-08-03 22:45:15 +0530

    Lab01

---
+++ Part02.kt
@@ -0,0 +1,68 @@
+// Q1
+abstract class Employee(){
+    fun pprint(){
+        println("I am Employee")
+    }
+}
+
+class TemporaryStaff(val name : String ,val department : String) : Employee() {
+    fun printTempDetails() {
+        println("==Q1==")
+        println("Name: ${name}, Department: ${department}")
+    }
+}
+
+class PermanentStaff(val name : String, val age : Int, val department: String, val status: String) : Employee() {
+    fun printDetails() {
+        println("Name: ${name}, Age: ${age}, Department: ${department}, Status: ${status}")
+    }
+}
+
+// Q2
+interface Shape {
+    fun setup()
+    fun draw()
+}
+
+class Circle() : Shape {
+    override fun setup() {
+        println()
+        println("==Q2==")
+        println("I am a Circle. Setting it up!")
+    }
+
+    override fun draw() {
+        println("Drawing a Circle")
+    }
+}
+
+class Square() : Shape {
+    override fun setup() {
+        println("I am a Circle. Setting it up!")
+    }
+
+    override fun draw() {
+        println("Drawing a Circle")
+    }
+}
+
+fun main() {
+    // Q1
+    val t1 = TemporaryStaff("Dimagi", "DS")
+    t1.printTempDetails()
+    t1.pprint()
+
+    val p1 = PermanentStaff("Thilini", 27, "IT", "LIC")
+    p1.printDetails()
+    p1.pprint()
+
+
+    // Q2
+    val c = Circle()
+    c.setup()
+    c.draw()
+
+    val s = Square()
+    s.setup()
+    s.draw()
+}
